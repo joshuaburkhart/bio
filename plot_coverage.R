f04e16f7952546c763d152589a249290ed0ad1ee
Joshua Burkhart <jburkhar@uoregon.edu>
2013-02-20 09:08:06 -0800

    adding plot_coverage.R

---
+++ plot_coverage.R
@@ -0,0 +1,4 @@
+.libPaths("/home13/jburkhar/tmp/")
+library("plotrix")
+data = read.table("stats.txt", header=TRUE)
+weighted.hist(data$short1_cov,data$lgth,breaks=0:100,col="dark blue",plot=TRUE,freq=TRUE,xlim=c(0,100),ylab="Frequency",xlab="Coverage")
