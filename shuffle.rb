17abcf586abedc13313609a43cfa94e23faa3d58
Joshua Burkhart <jburkhar@uoregon.edu>
2013-02-28 11:13:11 -0800

    ..

--- shuffle.rb
+++ shuffle.rb
@@ -7,9 +7,9 @@
 unshuffled_filename1 = ARGV[0]
 unshuffled_filename2 = ARGV[1]
 seq_id = ARGV[2]
-unshuffled_filename1.match(/^.*\/(\w+?)\.\w+$/)
+unshuffled_filename1.match(/^(.*\w+)\.\w+$/)
 name1 = $1
-unshuffled_filename2.match(/^.*\/(\w+?)\.\w+$/)
+unshuffled_filename2.match(/^(.*\w+)\.\w+$/)
 name2 = $1
 shuf_filehandl = File.open("#{name1}-#{name2}-shuf.fastq","w")
 
