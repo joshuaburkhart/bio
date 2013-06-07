c4ec125d2bc57441c2d700a0188e06673e704d01
Joshua Burkhart <burkhart.joshua@gmail.com>
2013-06-07 14:53:58 -0700

    ..updating foreign matcher example usage

--- ma_foreign_matcher.sh
+++ ma_foreign_matcher.sh
@@ -2,6 +2,8 @@
 
 #Usage: ma_foreign_matcher.sh <ma_intersect file> <foreign file indexed by gene>
 
+#Example: ma_foreign_matcher.sh KC-WI.txt.pos.csv-WI-WIOB.txt.neg.csv.intsct.csv contig_summaries.csv
+
 TMP=$(date | tr ' ' '-')
 cat $1 | while read LINE
 do GENE=$(echo $LINE | awk -F' ' '{print $1}')
