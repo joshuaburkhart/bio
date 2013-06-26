e8ab8f5b0351157dff745682bab8f9498436cfb9
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-06-26 14:35:13 -0700

    adding manhattan script

---
+++ manhattan.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+#Usage: manhattan.rb <coordinate input file> <threshold>
+#
+#Example: manhattan.rb WI-WIOB.minus-KC-WI.minus.combined 4
+
+infile = ARGV[0]
+threshold = Float(ARGV[1])
+
+inhandl = File.open(infile,"r")
+pos_outhandl = File.open("#{infile}.manhattan.pos","w")
+neg_outhandl = File.open("#{infile}.manhattan.neg","w")
+
+while(line = inhandl.gets)
+    x = Float(line.split(/ /)[0])
+    y = Float(line.split(/ /)[1])
+    if(x > 0 && y > 0 && x + y > threshold)
+        pos_outhandl.puts("#{x} #{y}")
+    elsif(x < 0 && y < 0 && x + y < threshold)
+        neg_outhandl.puts("#{x} #{y}")
+    end
+end
+
+inhandl.close
+pos_outhandl.close
+neg_outhandl.close
