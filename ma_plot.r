8e8f5a5c12a8a13f3e2864099c94acf6d8684e05
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-07-15 11:28:37 -0700

    counting quads in this script

--- ma_plot.r
+++ ma_plot.r
@@ -1,3 +1,28 @@
+
+count_quads <- function(T){
+    q1 = 0
+    q2 = 0
+    q3 = 0
+    q4 = 0
+    for(i in 1:length(T[,1])){
+        x = T[i,1] 
+        y = T[i,2]
+        if(x > 0 && y > 0){
+            q1 = q1 + 1 
+        }else if(x < 0 && y > 0){
+            q2 = q2 + 1
+        }else if(x < 0 && y < 0){
+            q3 = q3 + 1
+        }else if(x > 0 && y < 0){
+            q4 = q4 + 1
+        }else{
+            print("ERROR")
+            return(c(-1,-1,-1,-1))
+        }
+    }   
+    return(c(q1,q2,q3,q4))
+}
+
 A = read.table("WI-WIOB.minus-KC-WI.minus.combined")
 P = read.table("WI-WIOB.minus-KC-WI.minus.combined.manhattan.pos")
 N = read.table("WI-WIOB.minus-KC-WI.minus.combined.manhattan.neg")
@@ -7,7 +32,8 @@ points(P,pch=16,col="green")
 abline(a=0,b=0)
 abline(v=0)
 grid()
-legend("topright",legend=1197)
-legend("topleft",legend=235)
-legend("bottomleft",legend=2191)
-legend("bottomright",legend=1137)
+q_counts = count_quads(A)
+legend("topright",legend=q_counts[1])
+legend("topleft",legend=q_counts[2])
+legend("bottomleft",legend=q_counts[3])
+legend("bottomright",legend=q_counts[4])
