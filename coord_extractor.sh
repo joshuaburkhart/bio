849f4f2a8e430f3c661adcaa786c81e1b3731149
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-07-31 15:42:56 -0700

    fixing for multiple combinations..

--- coord_extractor.sh
+++ coord_extractor.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#Usage: coord_extractor.sh <input file> <coordinate field index (indexed from 1, space delimited)>
+#Usage: coord_extractor.sh <input file> <coordinate field index (indexed from 1, space delimited)> <number of files to create>
 
-#Example: coord_extractor.sh KC-WI.txt.stripped-WI-WIOB.txt.stripped.intsct.csv 2
+#Example: coord_extractor.sh KC-WI.txt.stripped-WI-WIOB.txt.stripped.intsct.csv 2 3
 
-#extract odd # lines (i.e. 1,3,5...) 
-cat $1 | awk 'NR%2' | awk -v num=$2 -F' ' '{print $num}' > $1.odd_coords
+if [ -z $3 ]; then
+    NUM_F=2
+else
+    NUM_F=$3
+fi
 
-#extract even # lines (i.e. 2,4,6...)
-cat $1 | awk 'NR%2 - 1' | awk -v num=$2 -F' ' '{print $num}' > $1.even_coords
+for i in $(seq 1 $NUM_F)
+do
+    cat $1 | awk -v fn=$NUM_F -v icr=$(($i % $NUM_F)) 'NR % fn == 0 + icr' | awk -v col=$2 -F' ' '{print $col}' > intsct.$i
+done
+
