b12b657aac81bd663ddafd4eac3254e127e85708
Joshua Burkhart <burkhart.joshua@gmail.com>
2013-12-03 22:16:04 -0500

    adding universal version of unshuffle.rb

--- unshuffle_universal.rb
+++ unshuffle_universal.rb
@@ -1,26 +1,23 @@
 #!/usr/bin/ruby
 
-#Usage: ruby unshuffle.rb <path/to/shuffled/filename> <illumina hiseq2000 sequence identifier>
+#Usage: ruby unshuffle.rb <path/to/shuffled/filename> <sequence identifier>
 
 #Example: ruby unshuffle.rb /home11/mmiller/Wyeomyia/reads/combined/wy_prefiltered_reads.fastq @HWI-72047
 
 shuffled_filename = ARGV[0]
 seq_id = ARGV[1]
-shuffled_filename.match(/^.*\/(\w+?)\.\w+$/)
+shuffled_filename.match(/^.*\/(\w+)(\.\w*)*$/)
 unshuf_filehandl1 = File.open("#{$1}-unshuf_1.fastq","w")
 unshuf_filehandl2 = File.open("#{$1}-unshuf_2.fastq","w")
 
-#TODO
-#filter paired reads with Y indicators (low quality indicators)
-
 File.open(shuffled_filename,"r") do |shuffled_file|
 	while shuffled_file_line = shuffled_file.gets
-		if shuffled_file_line.match(/^#{seq_id}.* 1:[NY]:.*$/)
+		if shuffled_file_line.match(/^#{seq_id}.*$/)
 			unshuf_filehandl1.print shuffled_file_line #sequence id
 			unshuf_filehandl1.print shuffled_file.gets #raw sequence letters
 			unshuf_filehandl1.print shuffled_file.gets #+
 			unshuf_filehandl1.print shuffled_file.gets #quality score
-		elsif shuffled_file_line.match(/^#{seq_id}.* 2:[NY]:.*$/)
+		elsif shuffled_file_line.match(/^#{seq_id}.*$/)
 			unshuf_filehandl2.print shuffled_file_line #sequence id
 			unshuf_filehandl2.print shuffled_file.gets #raw sequence letters
 			unshuf_filehandl2.print shuffled_file.gets #+ line
