5c6fb894be88eada39f13e814cf9fd05d5f5115f
Joshua Burkhart <burkhart.joshua@gmail.com>
2014-04-18 14:07:17 -0400

    adding status indicator

--- idx_match.sh
+++ idx_match.sh
@@ -7,13 +7,15 @@ OUT_FILE=subsample_$(date "+%N" | tr ' ' '-')
 
 touch $OUT_FILE
 
-echo "writing results to \"$OUT_FILE\"..."
+echo -n "Matching..."
 
 cat $1 | while read LINE
 do
 	egrep "^$LINE" $2 >> $OUT_FILE
+    echo -n "."
 done
-
+echo ""
 sort -n $OUT_FILE > $OUT_FILE.sorted
 
-echo "sorted results stored in \"$OUT_FILE.sorted\"..."
+echo "Results written to \"$OUT_FILE\""
+echo "Sorted results written to \"$OUT_FILE.sorted\""
